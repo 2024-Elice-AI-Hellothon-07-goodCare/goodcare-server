879be7909bf1d485d262816cfb6b04904ca763d2
chyun7114 <chyun7114@naver.com>
2024-11-12 20:07:55 +0900

    labtop to Desktop

--- src/main/java/com/goodcare/server/file/controller/FileController.java
+++ src/main/java/com/goodcare/server/file/controller/FileController.java
@@ -6,7 +6,6 @@ import com.goodcare.server.global.response.ApiResponse;
 import com.goodcare.server.global.response.Status;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,6 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/files")
 @RequiredArgsConstructor
-@Slf4j
 public class FileController {
 
     @Autowired
