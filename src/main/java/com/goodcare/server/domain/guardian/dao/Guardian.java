9eeede0107fa29b3719da40586dc9d0473010121
chyun7114 <chyun7114@naver.com>
2024-11-14 21:48:14 +0900

    fix:gurdian table 누락 수정

--- src/main/java/com/goodcare/server/domain/guardian/dao/Guardian.java
+++ src/main/java/com/goodcare/server/domain/guardian/dao/Guardian.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Entity
-@Table(name="caregiver")
+@Table(name="guardian")
 public class Guardian {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
