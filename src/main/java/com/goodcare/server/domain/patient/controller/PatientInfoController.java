4ba6b5810b8295ca4284d75f10b3690546ca2535
chyun7114 <chyun7114@naver.com>
2024-11-17 21:58:06 +0900

    브랜치 설정 : PR 만들 준비

--- src/main/java/com/goodcare/server/domain/patient/controller/PatientInfoController.java
+++ src/main/java/com/goodcare/server/domain/patient/controller/PatientInfoController.java
@@ -5,6 +5,7 @@ import com.goodcare.server.domain.patient.service.PatientInfoService;
 import com.goodcare.server.global.response.ApiResponse;
 import com.goodcare.server.global.response.Status;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/patient/info")
+@Tag(name = "Patient_INFO", description = "Patient DB 관련 api")
 @AllArgsConstructor
 public class PatientInfoController {
 
